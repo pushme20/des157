9a0ad70148475ec4b9c62f5cba2ee7639aed048b
Anwana Ntofon <aontofon@ucdavis.edu>
2017-01-17 13:45:07 -0800

    Changing file directory and add script
    moved files around to refine file structure and added script

---
+++ studio1/script.js
@@ -0,0 +1,14 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("this is a js comment");
+
+function setup() {
+  createCanvas(800, 250);
+  background(255);
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 50,50);
+}
