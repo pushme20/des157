2a25c131d63b4c1e55fb933c64f2cf9704c3c7c0
Anwana Ntofon <aontofon@ucdavis.edu>
2017-01-19 01:55:06 -0800

    Processing to JS Conversion
    concentric circles now span 8 rows and 1 column now. processing code
    converted to js syntax

--- studio1/script.js
+++ studio1/script.js
@@ -26,10 +26,10 @@ var y = 0; //initializing variable
 function draw() {
 
     background(0, 0, 255); //blue background
-    frameRate(10); //slowed down frame rate to 10 fps
+//    frameRate(10); //slowed down frame rate to 10 fps
 
-    for (int row_movement = 0; row_movement < 500; row_movement += 100) {
-        for (int col_movement = 0; col_movement < 500; col_movement += 100) {
+    for (var row_movement = 0; row_movement < 800; row_movement += 100) {
+        for (var col_movement = 0; col_movement < 100; col_movement += 100) {
             noStroke(); //removes stroke from circles
             fill(255, 0, 0); //red ring
             ellipse(50 + row_movement, 50 + col_movement, 95, 95); //outermost ring (which gets smaller and smaller)
@@ -52,13 +52,13 @@ function draw() {
         } //inner for loop is for column movement (y-axis)
     } //outer for loop is for row movement (x-axis)
 
-    strokeWeight(3); //thicker stroke
-    stroke(255, 255, 255); //white line
-    line(0, y, 500, y); //horizontal line
-    y = y + 5; //the amount of units that the line moves on the y-axis
+//    strokeWeight(3); //thicker stroke
+//    stroke(255, 255, 255); //white line
+//    line(0, y, 500, y); //horizontal line
+//    y = y + 5; //the amount of units that the line moves on the y-axis
 
     if (y == 50) {
-        for (int row1 = 0; row1 < 450; row1 += 100) {
+        for (var row1 = 0; row1 < 450; row1 += 100) {
             fill(153, 0, 204); //purple center
             ellipse(50 + row1, 50, 15, 15); //selects the center shape within the x-y plane
         }
@@ -66,30 +66,41 @@ function draw() {
     //the same code structure is used for the subsequent "if-for" combinations
 
     if (y == 150) {
-        for (int row2 = 0; row2 < 450; row2 += 100) {
+        for (var row2 = 0; row2 < 450; row2 += 100) {
             fill(153, 0, 204); //purple center
             ellipse(50 + row2, 50 + 100, 15, 15);
         }
     }
 
     if (y == 250) {
-        for (int row3 = 0; row3 < 450; row3 += 100) {
+        for (var row3 = 0; row3 < 450; row3 += 100) {
             fill(153, 0, 204); //purple center
             ellipse(50 + row3, 50 + 200, 15, 15);
         }
     }
 
     if (y == 350) {
-        for (int row4 = 0; row4 < 450; row4 += 100) {
+        for (var row4 = 0; row4 < 450; row4 += 100) {
             fill(153, 0, 204); //purple center
             ellipse(50 + row4, 50 + 300, 15, 15);
         }
     }
 
     if (y == 450) {
-        for (int row5 = 0; row5 < 450; row5 += 100) {
+        for (var row5 = 0; row5 < 450; row5 += 100) {
             fill(153, 0, 204); //purple center
             ellipse(50 + row5, 50 + 400, 15, 15);
         }
+    }
+    
+    if (y == 550) {
+        for (var row6 = 0; row6 < 450; row6 += 100) {
+            fill(153, 0, 204); //purple center
+            ellipse(50 + row6, 50 + 500, 15, 15);
+        }
+    }
+    
+    if(mouseIsClicked) {
+        background(255);
     }
 } //the ellipse function brings about the illusion of concentric circles
