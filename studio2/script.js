37bc8b3fcab9756325e7878075168c7e1610dbf4
Anwana Ntofon <aontofon@scc2102-5.ucdavis.edu>
2017-01-26 15:14:55 -0800

    JS for form submission

--- studio2/script.js
+++ studio2/script.js
@@ -4,5 +4,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
     // all other js here
+    document.madlibs.onsubmit = processForm;
+
+    function processForm() {
+        var question1 = document.madlibs.question1.value;
+        if (question1 == "Yesterday") {
+            console.log("Submit successful");
+        }
+
+
+
+
+
+
+
+
+    }
+
+
 
 });
