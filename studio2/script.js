f4cc71a6fc051ba515f861ab49aa4d36c632eadf
Anwana Ntofon <aontofon@scc2102-5.ucdavis.edu>
2017-01-26 16:06:05 -0800

    Additional variables added
    Variables added to store value from each of the input fields in html.
    condition to determine if the submission was successful.

--- studio2/script.js
+++ studio2/script.js
@@ -8,8 +8,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.madlib.onsubmit = processForm;
 
     function processForm() {
-        var question1 = document.madlib.name.value;
-        if (question1 == "Yesterday") {
+        var question1 = document.madlib.q1.value;
+        var question2 = document.madlib.q2.value;
+        var question3 = document.madlib.q3.value;
+        var question4 = document.madlib.q4.value;
+        var question5 = document.madlib.q5.value;
+
+        if (question1 == "a") {
+            console.log("Submit successful");
+        }
+        if (question2 == "b") {
+            console.log("Submit successful");
+        }
+        if (question3 == "c") {
+            console.log("Submit successful");
+        }
+        if (question4 == "d") {
+            console.log("Submit successful");
+        }
+        if (question5 == "e") {
             console.log("Submit successful");
         }
 
