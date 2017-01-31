5c1b40064866a91a3811c3d1fd0c68fc3558817a
Anwana Ntofon <aontofon@ucdavis.edu>
2017-01-30 17:31:59 -0800

    Error detection for empty or incomplete fields
    Reveals gameshow elements when all fields are filled

--- studio2/script.js
+++ studio2/script.js
@@ -15,9 +15,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var field5 = document.gameshow.field5.value;
         var field6 = document.gameshow.field6.value;
 
+        // document.getElementById('hide-regis').style.visibility = "hidden";
+        // var show_gameshow = document.GetElementById('hide-regis').style.visibility;
+
         if (field1 == "" && field2 == "" && field3 == "" && field4 == "" && field5 == "" && field6 == "") {
             alert("If you would like to play, please fill out all fields of the form.");
-
+        } else if (field1 == "" || field2 == "" || field3 == "" || field4 == "" || field5 == "" || field6 == "") {
+            alert("You cannot play while there are empty fields.");
+        } else {
+            document.getElementById('hide-regis').style.visibility = "visible";
         }
         return false;
     }
