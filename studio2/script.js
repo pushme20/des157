e9fc3a4a53f19361b9733adc5692c93b45049c82
Anwana Ntofon <aontofon@ucdavis.edu>
2017-01-30 18:18:45 -0800

    Button functions and global vars
    Moved submit and reset button actions in seperate functions. Also made
    field vars global.

--- studio2/script.js
+++ studio2/script.js
@@ -6,25 +6,57 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // all other js here
 
     document.gameshow.onsubmit = processForm;
+    var field1 = document.gameshow.field1.value;
+    var field2 = document.gameshow.field2.value;
+    var field3 = document.gameshow.field3.value;
+    var field4 = document.gameshow.field4.value;
+    var field5 = document.gameshow.field5.value;
+    var field6 = document.gameshow.field6.value;
 
     function processForm() {
-        var field1 = document.gameshow.field1.value;
-        var field2 = document.gameshow.field2.value;
-        var field3 = document.gameshow.field3.value;
-        var field4 = document.gameshow.field4.value;
-        var field5 = document.gameshow.field5.value;
-        var field6 = document.gameshow.field6.value;
 
-        // document.getElementById('hide-regis').style.visibility = "hidden";
-        // var show_gameshow = document.GetElementById('hide-regis').style.visibility;
 
+
+        // var reset = document.getElementById("reset_button").value;
+
+
+        // } else {
+        //     document.getElementById('hide-regis').style.visibility = "visible";
+        //     var addClass = document.getElementById('regis-pic');
+        //     addClass.classList.add("spinning-regis");
+        // }
+
+        // if (reset) {
+        //     reset_me();
+        // }
+        // if (reset) {
+        //     document.getElementById('hide-regis').style.visibility = "hidden";
+        //     var removeClass = document.getElementById('regis-pic');
+        //     removeClass.classList.remove("spinning-regis");
+        // }
+
+
+
+
+        return false;
+    }
+
+    document.getElementById('submit_button').onclick = function() {
         if (field1 == "" && field2 == "" && field3 == "" && field4 == "" && field5 == "" && field6 == "") {
             alert("If you would like to play, please fill out all fields of the form.");
         } else if (field1 == "" || field2 == "" || field3 == "" || field4 == "" || field5 == "" || field6 == "") {
             alert("You cannot play while there are empty fields.");
         } else {
             document.getElementById('hide-regis').style.visibility = "visible";
+            var addClass = document.getElementById('regis-pic');
+            addClass.classList.add("spinning-regis");
         }
-        return false;
-    }
+    };
+
+    document.getElementById('reset_button').onclick = function() {
+        document.getElementById('hide-regis').style.visibility = "hidden";
+        var removeClass = document.getElementById('regis-pic');
+        removeClass.classList.remove("spinning-regis");
+    };
+
 });
