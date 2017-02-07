7fa045e7ccfa558dcc4265a3ccb26f2b0410c65e
Anwana Ntofon <aontofon@cruess207-13.ucdavis.edu>
2017-02-07 11:57:50 -0800

    Hover effect for first image w/JS

--- studio3/script.js
+++ studio3/script.js
@@ -1,4 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
+    var first_img=document.getElementById('first-img');
+
+    // first_img.addEventListener('mouseup', showText);
+    first_img.addEventListener('mouseover', mouseWentIn);
+    first_img.addEventListener('mouseout', mouseWentOut);
+
+    function mouseWentIn() {
+      first_img.style.opacity = 0.5;
+      first_img.src="calcium_pills_ps_compressor.jpg";
+    }
+    function mouseWentOut() {
+      first_img.style.opacity = 1;
+      first_img.src="calcium_pills_ps_compressor.jpg";
+  // first_img.innerHTML = "Did it work?";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 });
