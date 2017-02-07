0449ed5db458667ee40dc818c26db0e17d23438d
Anwana Ntofon <aontofon@cruess207-13.ucdavis.edu>
2017-02-07 12:42:01 -0800

    Add emoji stylesheet & click works

--- studio3/script.js
+++ studio3/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    var first_img=document.getElementById('first-img');
-    var second_img=document.getElementById('second-img');
-    var third_img=document.getElementById('third-img');
-    var fourth_img=document.getElementById('fourth-img');
-    var fifth_img=document.getElementById('fifth-img');
+    var first_img = document.getElementById('first-img');
+    var second_img = document.getElementById('second-img');
+    var third_img = document.getElementById('third-img');
+    var fourth_img = document.getElementById('fourth-img');
+    var fifth_img = document.getElementById('fifth-img');
 
 
     // first_img.addEventListener('mouseup', showText);
@@ -25,65 +25,78 @@ document.addEventListener("DOMContentLoaded", function(event) {
     fifth_img.addEventListener('mouseout', mouseWentOut5);
 
 
+    first_img.addEventListener('click', function() {
+        document.getElementById('msg1').style.visibility = "visible";
+    });
+
+    // second_img.addEventListener('click', function() {
+    //     document.getElementById('msg2').style.visibility = "visible";
+    // });
+    //
+    // third_img.addEventListener('click', function() {
+    //     document.getElementById('msg1').style.visibility = "visible";
+    // });
+    //
+    // fourth_img.addEventListener('click', function() {
+    //     document.getElementById('msg1').style.visibility = "visible";
+    // });
+    //
+    // fifth_img.addEventListener('click', function() {
+    //     document.getElementById('msg1').style.visibility = "visible";
+    // });
+
+
 
     function mouseWentIn1() {
-      first_img.style.opacity = 0.5;
-      first_img.src="calcium_pills_ps_compressor.jpg";
+        first_img.style.opacity = 0.5;
+        first_img.src = "calcium_pills_ps_compressor.jpg";
     }
+
     function mouseWentOut1() {
-      first_img.style.opacity = 1;
-      first_img.src="calcium_pills_ps_compressor.jpg";
-  // first_img.innerHTML = "Did it work?";
+        first_img.style.opacity = 1;
+        first_img.src = "calcium_pills_ps_compressor.jpg";
     }
 
     function mouseWentIn2() {
-      second_img.style.opacity = 0.5;
-      second_img.src="weights_ps_compressor.jpg";
+        second_img.style.opacity = 0.5;
+        second_img.src = "weights_ps_compressor.jpg";
     }
+
     function mouseWentOut2() {
-      second_img.style.opacity = 1;
-      second_img.src="weights_ps_compressor.jpg";
-  // first_img.innerHTML = "Did it work?";
+        second_img.style.opacity = 1;
+        second_img.src = "weights_ps_compressor.jpg";
+        // first_img.innerHTML = "Did it work?";
     }
 
     function mouseWentIn3() {
-      third_img.style.opacity = 0.5;
-      third_img.src="nike_shoe_ps_compressor.jpg";
+        third_img.style.opacity = 0.5;
+        third_img.src = "nike_shoe_ps_compressor.jpg";
     }
-    function mouseWentOut3() {
-      third_img.style.opacity = 1;
-      third_img.src="nike_shoe_ps_compressor.jpg";
 
+    function mouseWentOut3() {
+        third_img.style.opacity = 1;
+        third_img.src = "nike_shoe_ps_compressor.jpg";
     }
 
     function mouseWentIn4() {
-      fourth_img.style.opacity = 0.5;
-      fourth_img.src="foam_roller_ps_compressor_vert.jpg";
+        fourth_img.style.opacity = 0.5;
+        fourth_img.src = "foam_roller_ps_compressor_vert.jpg";
     }
+
     function mouseWentOut4() {
-      fourth_img.style.opacity = 1;
-      fourth_img.src="foam_roller_ps_compressor_vert.jpg";
+        fourth_img.style.opacity = 1;
+        fourth_img.src = "foam_roller_ps_compressor_vert.jpg";
 
     }
 
     function mouseWentIn5() {
-      fifth_img.style.opacity = 0.5;
-      fifth_img.src="workout_gear_ps_compressor.jpg";
+        fifth_img.style.opacity = 0.5;
+        fifth_img.src = "workout_gear_ps_compressor.jpg";
     }
+
     function mouseWentOut5() {
-      fifth_img.style.opacity = 1;
-      fifth_img.src="workout_gear_ps_compressor.jpg";
+        fifth_img.style.opacity = 1;
+        fifth_img.src = "workout_gear_ps_compressor.jpg";
 
     }
-
-
-
-
-
-
-
-
-
-
-
 });
