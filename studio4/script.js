221027f34a0690ba6cafd6231b1f439e75130814
Anwana Ntofon <aontofon@ucdavis.edu>
2017-02-12 14:29:19 -0800

    Working with displaying and hiding audio ctrls

--- studio4/script.js
+++ studio4/script.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-  });
+    var flag1 = document.getElementById('center-button');
+    var me = document.getElementById('hide-left');
+    var flag3 = document.getElementById('hide-right');
+    var showControls = document.getElementById('hide-sound');
+
+    var Timer;
+
+    flag1.addEventListener('click', playAudio);
+
+    // function playAudio() {
+    //     var sound = document.getElementsByTagName("audio")[0];
+    //     sound.load();
+    //     sound.play()
+    //
+    //     showControls.style.display = "inline-block";
+    //
+    // }
+});
