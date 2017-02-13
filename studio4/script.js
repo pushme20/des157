c8022ba68f6293d3a711f987818d195144c52f7d
Anwana Ntofon <aontofon@ucdavis.edu>
2017-02-13 15:28:32 -0800

    All buttons functioning properly

--- studio4/script.js
+++ studio4/script.js
@@ -1,43 +1,86 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
+    var count = 0;
+    var Timer;
 
-    var flag1 = document.getElementById('center-button');
-    var me = document.getElementById('hide-left');
-    var flag3 = document.getElementById('hide-right');
-    var showControls = document.getElementById('hide-sound');
+    var center_button = document.getElementById('center-button');
+    var left_button = document.getElementById('hide-left');
+    var right_button = document.getElementById('hide-right');
+    var top_button = document.getElementById('hide-top');
+    var bot_button = document.getElementById('hide-bot');
 
     /*EASTER EGG INDICATORS*/
-    var right_easter = document.getElementById('left-easter');
-    var left_easter = document.getElementById('right-easter');
+    var right_easter = document.getElementById('right-easter');
+    var left_easter = document.getElementById('left-easter');
     var top_easter = document.getElementById('top-easter');
     var bot_easter = document.getElementById('bot-easter');
 
-    var Timer;
+    var showControls = document.getElementById('hide-sound');
 
-    flag1.addEventListener('click', playAudio);
-    flag1.addEventListener('mouseover', noteOn);
-    flag1.addEventListener('mouseout', noteOff);
 
-    flag3.addEventListener('click', easterEgg);
+    center_button.addEventListener('click', playAudio);
+    center_button.addEventListener('mouseover', noteOn);
+    center_button.addEventListener('mouseout', noteOff);
 
+    right_button.addEventListener('click', showRightButton);
     right_easter.addEventListener('dblclick', changeTo3);
 
+    top_button.addEventListener('click', showRightButton);
+    top_easter.addEventListener('dblclick', changeTo12);
+
+    left_button.addEventListener('click', showRightButton);
+    left_easter.addEventListener('dblclick', changeTo9);
+
+    bot_button.addEventListener('click', showRightButton);
+    bot_easter.addEventListener('dblclick', changeTo6);
+
     function changeTo3() {
+        count++;
+        console.log(count);
+        top_button.style.visibility = "visible";
         return right_easter.innerHTML = "3";
     }
 
+    function changeTo12() {
+        count++;
+        console.log(count);
+        left_button.style.visibility = "visible";
+        return top_easter.innerHTML = "12";
+    }
+
+    function changeTo9() {
+        count++;
+        console.log(count);
+        bot_button.style.visibility = "visible";
+        return left_easter.innerHTML = "9";
+    }
+
+    function changeTo6() {
+        count++;
+        console.log(count);
+        return bot_easter.innerHTML = "6";
+    }
+
+    if (count == 4) {
+        changeToInf();
+    }
+
+    function changeToInf() {
+        return center_button.innerHTML = "Hi";
+    }
+
     function noteOn() {
-        return flag3.style.visibility = 'visible';
+        return right_button.style.visibility = 'visible';
     }
 
 
     function noteOff() {
-        return flag3.style.visibility = 'hidden';
+        return right_button.style.visibility = 'hidden';
     }
 
-    function easterEgg() {
+    function showRightButton() {
 
-        return flag3.style.visibility = 'visible';
+        return right_button.style.visibility = 'visible';
     }
 
     function playAudio() {
@@ -96,4 +139,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //     var bod = document.getElementById('red');
     //     bod.style.backgroundColor = "red";
     // }
+
 });
