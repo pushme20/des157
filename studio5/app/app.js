131f46925e2569e1221e067af6c267d74d8c0c83
Anwana Ntofon <aontofon@ucdavis.edu>
2017-03-06 16:02:38 -0800

    Created continents array of objects
    Will be used to extract certain info from continents/countries

--- studio5/app/app.js
+++ studio5/app/app.js
@@ -30,3 +30,53 @@ app.config(['$routeProvider', function($routeProvider) {
             redirectTo: '/home'
         });
 }]);
+
+app.controller('appController', ['$scope', function($scope) {
+    $scope.continents = [{
+            name: "Europe",
+            country1: "Sweden",
+            lang1: "Swedish",
+            country2: "Portugal",
+            lang2: "Portuguese"
+        },
+        {
+            name: "Asia",
+            country1: "Nepal",
+            lang1: "Nepali",
+            country2: "Thailand",
+            lang2: "Thai"
+        },
+        {
+            name: "Africa",
+            country1: "Uganda",
+            lang1: "English",
+            lang1_2: "Swahili",
+            lang1_3: "Luganda",
+            country2: "Sierra Leone",
+            lang2: "English",
+            lang2_1: "Bengali"
+        },
+        {
+            name: "South America",
+            country1: "Argentina",
+            lang1: "Spanish",
+            country2: "Peru",
+            lang2: "Spanish",
+            lang2_1: "Aymara",
+            lang2_2: "Quechua"
+        },
+        {
+            name: "Australia",
+            country1: "Sweden",
+            lang1: "English",
+            lang1_2: "Australian English"
+        },
+        {
+            name: "North America",
+            country1: "California",
+            country2: "New York",
+            nativeLanguage: "English"
+
+        },
+    ]
+}]);
