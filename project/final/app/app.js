c6216c4fbab375c2406ad68f9a6d88d600fffc97
Anwana Ntofon <aontofon@scc2102-3.ucdavis.edu>
2017-03-14 15:56:33 -0700

    Final folder created and linked

---
+++ project/final/app/app.js
@@ -0,0 +1,124 @@
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'angularCSS']);
+
+// app.controller('appController', function($scope) {
+//     $scope.message = "hello";
+//     $scope.talk = "eat";
+// });
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    // $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html'
+        })
+        .when('/explore', {
+            templateUrl: 'views/explore.html',
+            controller: 'appController'
+        })
+        .when('/discover', {
+            templateUrl: 'views/discover.html',
+            controller: 'appController'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'appController',
+            css: 'css/about.css'
+        })
+        .when('/help', {
+            templateUrl: 'views/help.html',
+            controller: 'appController'
+        })
+        .when('/success-page', {
+            templateUrl: 'views/success-page.html',
+            controller: 'appController',
+
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
+
+app.controller('appController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+
+    $http.get('data/continents.json').then(function(response) {
+        $scope.continents = response.data;
+    });
+
+    $scope.sendMessage = function() {
+        $location.path('success-page'); //sends us to contact-success page
+    };
+
+}]);
+// $scope.continents = [{
+//         name: "Europe",
+//         country1: "Sweden",
+//         lang1: "Swedish",
+//         country2: "Portugal",
+//         lang2: "Portuguese"
+//     },
+//     {
+//         name: "Asia",
+//         country1: "Nepal",
+//         lang1: "Nepali",
+//         country2: "Thailand",
+//         lang2: "Thai"
+//     },
+//     {
+//         name: "Africa",
+//         country1: "Uganda",
+//         lang1: "English",
+//         lang1_2: "Swahili",
+//         lang1_3: "Luganda",
+//         country2: "Sierra Leone",
+//         lang2: "English",
+//         lang2_1: "Bengali"
+//     },
+//     {
+//         name: "South America",
+//         country1: "Argentina",
+//         lang1: "Spanish",
+//         country2: "Peru",
+//         lang2: "Spanish",
+//         lang2_1: "Aymara",
+//         lang2_2: "Quechua"
+//     },
+//     {
+//         name: "Australia",
+//         country1: "Sweden",
+//         lang1: "English",
+//         lang1_2: "Australian English"
+//     },
+//     {
+//         name: "North America",
+//         country1: "California",
+//         country2: "New York",
+//         nativeLanguage: "English"
+//
+//     },
+// ]
+
+
+
+
+
+
+// app.controller('successController', ['$scope', '$location',
+//     function($scope, $location) {
+//         $scope.sendMessage = function() {
+//             $location.path('success-page'); //sends us to contact-success page
+//         };
+//     }
+// ]);
+
+//   '$location', function($scope, $location) {
+//     $scope.sendMessage = function() {
+//         $location.path('contact-success'); //sends us to contact-success page
+//
+//     };
+// tact-success page
+//
+//     };
+//     tact - success page
+//
+// };
---
+++ project/final/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    // var explore = document.getElementById('explore');
+    // var about = document.getElementById('about');
+    // var help = document.getElementById('help');
+    // var content_box = document.getElementById('content_box');
+    //
+    // explore.addEventListener("click", showDownExplore);
+    // explore.addEventListener("dblclick", showUpExplore);
+    // about.addEventListener("click", showDownAbout);
+    // about.addEventListener("dblclick", showUpAbout);
+    // help.addEventListener("click", showDownHelp);
+    // help.addEventListener("dblclick", showUpHelp);
+    //
+    // function showDownExplore() {
+    //     explore.innerHTML = "Explore&nbsp;&nbsp;&nbsp;&nbsp;&#9660";
+    //     content_box.style.visibility = "hidden";
+    // }
+    //
+    // function showUpExplore() {
+    //     explore.innerHTML = "Explore&nbsp;&nbsp;&nbsp;&nbsp;&#9654";
+    //     content_box.style.visibility = "visible";
+    // }
+    //
+    //
+    //
+    // function showDownAbout() {
+    //     about.innerHTML = "About&nbsp;&nbsp;&nbsp;&nbsp;&#9660";
+    //     content_box.style.visibility = "hidden";
+    // }
+    //
+    // function showUpAbout() {
+    //     about.innerHTML = "About &nbsp;&nbsp;&nbsp;&nbsp;&#9654";
+    //     content_box.style.visibility = "visible";
+    // }
+    //
+    // function showDownHelp() {
+    //     help.innerHTML = "Help&nbsp;&nbsp;&nbsp;&nbsp;&#9660";
+    //
+    //     content_box.style.visibility = "hidden";
+    // }
+    //
+    // function showUpHelp() {
+    //     help.innerHTML = "Help&nbsp;&nbsp;&nbsp;&nbsp;&#9654";
+    //     content_box.style.visibility = "visible";
+    // }
+
+    // var slideSource = document.getElementById('slideSource');
+    //
+    // // document.getElementById('handle').onclick = function() {
+    // //     slideSource.classList.toggle('fade');
+    // // }
+    // document.addEventListener('click', function() {
+    //     slideSource.classList.toggle('fade');
+    // });
+    //
+    // var slideSource2 = document.getElementById('slideSource2');
+    //
+    // document.getElementById('handle2').onclick = function() {
+    //     slideSource2.classList.toggle('fade');
+    // }
+    //
+    // var slideSource3 = document.getElementById('slideSource3');
+    // 
+    // document.getElementById('handle3').onclick = function() {
+    //     slideSource3.classList.toggle('fade');
+    // }
+});
