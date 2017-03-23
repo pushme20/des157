43ad2ed8c05b39a63053844c0e78c8073acff9e6
Anwana Ntofon <aontofon@ucdavis.edu>
2017-03-22 19:25:57 -0700

    Took text from other script file and put into help
    Commented out other script file

--- project/final/app/script.js
+++ project/final/app/script.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
     console.log("DOM fully loaded and parsed");
 
     $("#firstQ").hide();
+    // $("#content1").hide();
+
 
     $("#questionOne").click(function() {
         $("#firstQ").show("200");
-        $("#content1").html("To develop cultural awareness and empower people to learn about each other. This site is designed to foster exploration, growth, and a welcoming community for all types of individuals.");
+        $("#content1").html("To develop  awareness and empower people to learn about each other. This site is designed to foster exploration, growth, and a welcoming community for all types of individuals.");
     });
 
     $("#questionTwo").click(function() {
-        $("#content1").html("questionThis is the second question questionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second question");
+        $("#firstQ").show("200");
+        $("#content1").html("Click the logo in the upper right hand corner to be taken to the main menu. You may then select 'Explore' or 'Discover' to decide which culture you wish to learn about. 'Explore' is meant for those who are unsure of where to begin. Start there for a more wholesome experience!");
     });
 
     $("#questionThree").click(function() {
+        $("#firstQ").show("200");
         $("#content1").html("questionThis is the second question questionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second questionquestionThis is the second question");
     });
 });
